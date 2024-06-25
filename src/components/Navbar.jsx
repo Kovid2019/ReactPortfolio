2201b23aae8e54eb022f6987117036bd90a7085e
Kovid Kunduru <kovid@Kovids-MacBook-Air.local>
2024-06-26 03:03:30 +1000

    navbar issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Navbar = () => {
+  const [slideMenu, setSlideMenu] = useState(false);
+
   return (
     <div>
       <header>
@@ -9,12 +11,18 @@ export const Navbar = () => {
             <div>Kovid</div>
             <div className="line">FStack. Dev</div>
           </div>
-          <label htmlFor="hamburgerMenu">
+          <label
+            htmlFor="hamburgerMenu"
+            onClick={() => setSlideMenu(!slideMenu)}
+          >
             <i className="fa-solid fa-bars"></i>
           </label>
           <input type="checkbox" id="hamburgerMenu" />
-          <div className="menu">
-            <ul className="flex navigation">
+          <div className={slideMenu ? "menu slide" : "menu"}>
+            <ul
+              className="flex navigation"
+              onClick={() => setSlideMenu(!slideMenu)}
+            >
               <li>
                 <a href="#hero">Home</a>
               </li>
